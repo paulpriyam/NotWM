c441eb72d085852d26eee96a86baff7b92385c1a
paulpriyam <ppaul2204@gmail.com>
2023-06-18 15:52:54 +0530

    feature:- 1. added Item divider for recyclerView items

--- app/src/main/java/com/example/forgetnoting/ui/ReminderListFragment.kt
+++ app/src/main/java/com/example/forgetnoting/ui/ReminderListFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.forgetnoting.R
 import com.example.forgetnoting.adapter.ReminderAdapter
@@ -35,6 +37,15 @@ class ReminderListFragment : Fragment(R.layout.fragment_reminder_list) {
         binding.rvReminders.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = reminderAdapter
+            val itemDecoration = DividerItemDecoration(
+                requireContext(),
+                DividerItemDecoration.VERTICAL
+            )
+            ContextCompat.getDrawable(requireContext(), R.drawable.divider)
+                ?.let { itemDecoration.setDrawable(it) }
+            addItemDecoration(
+                itemDecoration
+            )
         }
 
         viewModel.reminders.observe(viewLifecycleOwner, Observer {
