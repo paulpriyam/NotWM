825b2eb7e94dadeec5eb561bbffc2ea06f15c6b6
paulpriyam <ppaul2204@gmail.com>
2023-06-21 17:13:06 +0530

    UI changes:- 1. Added navigation and UI changes.

--- app/src/main/java/com/example/forgetnoting/habit/ui/HabitListFragment.kt
+++ app/src/main/java/com/example/forgetnoting/habit/ui/HabitListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.forgetnoting.R
 import com.example.forgetnoting.databinding.FragmentHabitListBinding
 import com.example.forgetnoting.habit.viewmodel.HabitViewModel
@@ -28,5 +29,9 @@ class HabitListFragment : Fragment(R.layout.fragment_habit_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.fabAddHabit.setOnClickListener {
+            findNavController().navigate(R.id.action_habitListFragment_to_addHabitFragment)
+        }
     }
 }
